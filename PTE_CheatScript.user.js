ccf56428a0f2e634cd913d3d614949b10be5a6e5
leo7044 <cc.ta.leo7044@gmail.com>
2018-06-25 21:40:58 +0200

    decoded source-code

--- PTE_CheatScript.user.js
+++ PTE_CheatScript.user.js
@@ -1,13 +1,83 @@
 // ==UserScript==
 // @name        PTE_CheatScript
-// @version     2018.06.05
+// @version     2018.06.25
+// @author      leo7044
 // @description PTE_CheatScript
-// @downloadURL https://raw.githubusercontent.com/leo7044/CnC_TA/master/PTE_CheatScript.user.js
-// @updateURL   https://raw.githubusercontent.com/leo7044/CnC_TA/master/PTE_CheatScript.user.js
+// @downloadURL https://github.com/leo7044/CnC_TA/raw/master/PTE_CheatScript.user.js
+// @updateURL   https://github.com/leo7044/CnC_TA/raw/master/PTE_CheatScript.user.js
 // @include     http*://prodgame*.alliances.commandandconquer.com/320/index.aspx*
 // @include     http*://cncapp*.alliances.commandandconquer.com/320/index.aspx*
-// @author      leo7044 (https://github.com/leo7044)
 // @grant       none
 // ==/UserScript==
 
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('!4(){2 t=4(){4 t(){w{5(6.7.8.3().h().N()>0){s(2 e=!1,a=0;a<6.7.8.3().h().r().B;a++)5(6.7.8.3().h().r()[a].14==X("W==")){e=!0;R}5(e){2 n=6.7.8.3().C().Q().d;a=0;s(2 i D 6.7.8.3().O().P()<v&&9.c.b.f().o().m().k("/l E v"),n)n[i].F()<G&&9.c.b.f().o().m().k("/l H "+a),!0===n[i].I()&&9.c.b.f().o().m().k("/l J"),n[i].K().L()&&n[i].M(),a++}}g.u(t,q)}p(t){j.x(t)}}!4 e(){w{5("S"!=T 9&&9.c.b.f().U())V y t()}p(t){y 0!==j?j.x(t):g.z?z.Y(t):Z(t)}g.u(e,q)}()};!4(){5("/10/11"!=g.12.13){2 e=A.15("16");e.17="("+t.18()+")();",e.19="1a/1b",A.1c("1d")[0].1e(e)}}()}();',62,77,'||var|GetInstance|function|if|ClientLib|Data|MainData|qx||Init|core|||getApplication|window|get_Alliance||console|send|cheat|getChatWidget||getChat|catch|1e3|get_MemberDataAsArray|for||setTimeout|9999|try|log|void|opera|document|length|get_Cities|in|setcommandpoints|GetFullConditionInPercent|100|repairallpte|get_hasCooldown|resetmovecooldownpte|get_CityBuildingsData|get_HasCollectableBuildings|CollectAllResources|get_Id|get_Player|GetCommandPointCount|get_AllCities|break|undefined|typeof|getMenuBar|return|bGVvNzA0NA|atob|postError|GM_log|login|auth|location|pathname|Name|createElement|script|innerHTML|toString|type|text|javascript|getElementsByTagName|head|appendChild'.split('|'),0,{}))
+(function () {
+    var PTECheatMain = function ()
+    {
+        function PTECheatCreate()
+        {
+            try
+            {
+				var bases = ClientLib.Data.MainData.GetInstance().get_Cities().get_AllCities().d;
+				var i = 0;
+				if (ClientLib.Data.MainData.GetInstance().get_Player().GetCommandPointCount() < 9999)
+				{
+					qx.core.Init.getApplication().getChat().getChatWidget().send("/cheat setcommandpoints 9999");
+				}
+				for (var key in bases)
+				{
+					if (bases[key].GetFullConditionInPercent() < 100)
+					{
+						qx.core.Init.getApplication().getChat().getChatWidget().send("/cheat repairallpte " + i);
+					}
+					if (bases[key].get_hasCooldown() === true)
+					{
+						qx.core.Init.getApplication().getChat().getChatWidget().send("/cheat resetmovecooldownpte");
+					}
+					if (bases[key].get_CityBuildingsData().get_HasCollectableBuildings())
+					{
+						bases[key].CollectAllResources();
+					}
+					i++;
+				}
+				window.setTimeout(PTECheatCreate, 1000);
+            }
+            catch(e)
+            {
+                console.log(e);
+            }
+        }
+        function LoadExtension()
+        {
+            try
+            {
+                if (typeof(qx)!='undefined')
+                {
+                    if (!!qx.core.Init.getApplication().getMenuBar())
+                    {
+                        PTECheatCreate();
+                        return;
+                    }
+                }
+            }
+            catch (e)
+            {
+                if (console !== undefined) console.log(e);
+                else if (window.opera) opera.postError(e);
+                else GM_log(e);
+            }
+            window.setTimeout(LoadExtension, 1000);
+        }
+        LoadExtension();
+    };
+    function Inject()
+    {
+        if (window.location.pathname != ("/login/auth"))
+        {
+            var Script = document.createElement("script");
+            Script.innerHTML = "(" + PTECheatMain.toString() + ")();";
+            Script.type = "text/javascript";
+            document.getElementsByTagName("head")[0].appendChild(Script);
+        }
+    }
+    Inject();
+})();
