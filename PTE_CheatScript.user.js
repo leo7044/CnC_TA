77051db3ab6c455314f7d7c8f8c6beea59b386fa
leo7044 <cc.ta.leo7044@gmail.com>
2018-12-20 18:27:42 +0100

    Add files via upload

--- PTE_CheatScript.user.js
+++ PTE_CheatScript.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        PTE_CheatScript
-// @version     2018.10.12
+// @version     2018.12.20
 // @author      leo7044 (https://github.com/leo7044)
 // @description PTE_CheatScript
 // @downloadURL https://github.com/leo7044/CnC_TA/raw/master/PTE_CheatScript.user.js
@@ -10,74 +10,4 @@
 // @grant       none
 // ==/UserScript==
 
-(function () {
-    var PTECheatMain = function ()
-    {
-        function PTECheatCreate()
-        {
-            try
-            {
-				var bases = ClientLib.Data.MainData.GetInstance().get_Cities().get_AllCities().d;
-				var i = 0;
-				if (ClientLib.Data.MainData.GetInstance().get_Player().GetCommandPointCount() < 9999)
-				{
-					qx.core.Init.getApplication().getChat().getChatWidget().send("/cheat setcommandpoints 9999");
-				}
-				for (var key in bases)
-				{
-					/*if (!bases[key].get_IsGhostMode() && bases[key].GetFullConditionInPercent() < 100)
-					{
-						qx.core.Init.getApplication().getChat().getChatWidget().send("/cheat repairallpte " + i);
-					}*/
-					if (bases[key].get_hasCooldown() === true)
-					{
-						qx.core.Init.getApplication().getChat().getChatWidget().send("/cheat resetmovecooldownpte");
-					}
-					if (bases[key].get_CityBuildingsData().get_HasCollectableBuildings())
-					{
-						bases[key].CollectAllResources();
-					}
-					i++;
-				}
-				window.setTimeout(PTECheatCreate, 1000);
-            }
-            catch(e)
-            {
-                console.log(e);
-            }
-        }
-        function LoadExtension()
-        {
-            try
-            {
-                if (typeof(qx)!='undefined')
-                {
-                    if (!!qx.core.Init.getApplication().getMenuBar())
-                    {
-                        PTECheatCreate();
-                        return;
-                    }
-                }
-            }
-            catch (e)
-            {
-                if (console !== undefined) console.log(e);
-                else if (window.opera) opera.postError(e);
-                else GM_log(e);
-            }
-            window.setTimeout(LoadExtension, 1000);
-        }
-        LoadExtension();
-    };
-    function Inject()
-    {
-        if (window.location.pathname != ("/login/auth"))
-        {
-            var Script = document.createElement("script");
-            Script.innerHTML = "(" + PTECheatMain.toString() + ")();";
-            Script.type = "text/javascript";
-            document.getElementsByTagName("head")[0].appendChild(Script);
-        }
-    }
-    Inject();
-})();
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('!3(){4("/B/C"!=5.D.E){6 t=o.F("G");t.H="("+3(){3 i(){p{q(6 t=!1,e=0;e<7.8.9.b().r().s().I;e++)4(7.8.9.b().r().s()[e].J==K("L==")){t=!0;M}4(t){6 a=7.8.9.b().N().O().d;q(6 n P e=0,7.8.9.b().Q().R()<u&&2.c.f.g().h().j().k("/l S u"),a)!a[n].T()&&a[n].U()<V&&2.c.f.g().h().j().k("/l W "+e),!0===a[n].X()&&2.c.f.g().h().j().k("/l Y"),a[n].Z().10()&&a[n].11(),e++}5.v(i,w)}x(t){m.y(t)}}!3 t(){p{4("12"!=13 2&&2.c.f.g().14())15 z i()}x(t){z 0!==m?m.y(t):5.A?A.16(t):17(t)}5.v(t,w)}()}.18()+")();",t.19="1a/1b",o.1c("1d")[0].1e(t)}}();',62,77,'||qx|function|if|window|var|ClientLib|Data|MainData||GetInstance|core|||Init|getApplication|getChat||getChatWidget|send|cheat|console||document|try|for|get_Alliance|get_MemberDataAsArray||9999|setTimeout|1e3|catch|log|void|opera|login|auth|location|pathname|createElement|script|innerHTML|length|Name|atob|bGVvNzA0NA|break|get_Cities|get_AllCities|in|get_Player|GetCommandPointCount|setcommandpoints|get_IsGhostMode|GetFullConditionInPercent|100|repairallpte|get_hasCooldown|resetmovecooldownpte|get_CityBuildingsData|get_HasCollectableBuildings|CollectAllResources|undefined|typeof|getMenuBar|return|postError|GM_log|toString|type|text|javascript|getElementsByTagName|head|appendChild'.split('|'),0,{}))
